a87028c400989f8780634e03bc0cc072b7c445a7
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-07-12 11:41:16 +0500

    Setup react prject using vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>AskSunnah React Frontend</h1>
+      <p>Let's build something great!</p>
+    </div>
+  );
+}
+
+export default App;
