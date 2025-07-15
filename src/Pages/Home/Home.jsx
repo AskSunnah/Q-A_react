52b3e320bd074e29881585cd05e990121ffd3e0f
khaulanauman <khaulanauman@gmail.com>
2025-07-15 16:59:37 +0500

    optimized code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import Navbar from '../../Components/Navbar';
+import Footer from '../../Components/Footer';
 
-import Header from '../Components/Home/Header';
-import IntroSection from '../Components/Home/IntroSection';
-import RecentAnswers from '../Components/Home/RecentAnswers';
-import AskQuestionModal from '../Components/Home/AskQuestionModal';
+import Header from '../../Components/Home/Header';
+import IntroSection from '../../Components/Home/IntroSection';
+import RecentAnswers from '../../Components/Home/RecentAnswers';
+import AskQuestionModal from '../../Components/Home/AskQuestionModal';
 
-import { fetchAllFatwas } from "../api/fatwa"
+import { fetchAllFatwas } from "../../api/fatwa"
 
 
 function Home() {
